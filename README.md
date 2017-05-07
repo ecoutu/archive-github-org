a13e3f8bf8fd4f833195285c35b112dd74a92e02
Eric Coutu <eric.coutu@gmail.com>
2017-05-07 01:32:07 -0400

    Initial code commit

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # archive-github-org
-Shell script for archiving a github user/org
+
+Shell script for archiving a github user/org. 
+
+## Running
+
+This script depends on jq, curl and git:
+
+```bash
+sudo apt-get install -y jq curl git
+```
+
+Also, the aws command line tools for the uploading to s3 part (feel free to do what you wish with your archive, but 
+configuring s3 is beyond the scope of this).
+
+It accepts two positional arguments, github org and github token
+
+eg invocation: 
+  
+```bash
+./clone_org.sh <org> <gh_token>
+```
+
+## Disclaimer
+
+This almost certainly would have been easier to write, read, and understand had it been written in a higher level 
+language such as python.
+
+### But... why?
+
+If you are reading this then you know why. 
+
+#### Ok, seriously
+
+I needed something to backup / archive my personal projects that I keep stored on github. Once I started prodding at 
+[github's super awesome api](https://developer.github.com/v3/) using simple tools such as curl I figured it would be 
+fun to do a full implementation with said "unsophisticated" tools.
+
+##### ...
+
+Maybe you and I don't necessarily agree on what the meaning of "fun" is.
---
+++ clone_org.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+# This script depends on jq, curl and git
+#   sudo apt-get install -y jq curl git
+# It accepts two positional arguments, github org and github token
+#   eg invocation: ./clone_org.sh <org> <gh_token>
+
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+RED='\033[0;31m'
+BOLD_NORMAL='\033[1;39m'
+BOLD_GREEN='\033[1;32m'
+BOLD_YELLOW='\033[1;33m'
+BOLD_RED='\033[1;31m'
+END='\033[0m' # No Color
+
+org=$1
+token=$2
+s3_bucket=$3
+
+fetch_and_clone () {
+  local url=$1
+
+  echo "Fetching repos from ${url}"
+
+  {
+    curl -s --dump-header /dev/stderr -X GET ${url} 2>&3 | jq -r '.[].full_name' | \
+    while read repo_name; do
+      echo -e "${BOLD_NORMAL}$(printf '%-10s' Start:)"$(printf "%-100s\n" "${repo_name}" | tr " " -)"${END}"
+      mkdir -p ${repo_name}
+      pushd ${repo_name}
+      git init
+      git pull "https://${token}@github.com/${repo_name}.git"
+      success=$?
+      popd
+      if [ ${success} -eq 0 ]; then
+        echo -e "${BOLD_GREEN}$(printf "%-10s"  "Success:")"$(printf "%-100s" "${repo_name}" | tr " " -)"${END}"
+      else
+        echo -e "${BOLD_RED}$(printf "%-10s"  "Failure:")"$(printf "%-100s" "${repo_name}" | tr " " -)"${END}"
+      fi
+    done
+  } \
+  3>&1 1>&2 | grep -Po '(?<=Link:\s<)[^>]+(?=>;\s*rel="next")' | \
+  while read link; do
+    echo Found next link ${link}
+    fetch_and_clone ${link}
+  done
+}
+
+date_suffix=$(date +%s)
+url="https://api.github.com/orgs/${org}/repos?access_token=${token}&per_page=100"
+tmp_path="/tmp/${org}-gh-archive-${date_suffix}"
+archive_name="${org}-gh-archive-${date_suffix}.tar.gz"
+
+echo "Cloning repos to ${tmp_path}"
+
+mkdir "${tmp_path}"
+pushd "${tmp_path}"
+
+fetch_and_clone ${url}
+
+echo "Archiving repos to ./${archive_name}"
+tar czvf "../${archive_name}" .
+
+echo "Sending archive ${archive_name} to s3 bucket ${s3_bucket}"
+aws s3 cp "../${archive_name}" "${s3_bucket}"
+
+popd
+
+rm -rf "/tmp/${archive_name}" "${tmp_path}"
